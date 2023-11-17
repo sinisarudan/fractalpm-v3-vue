65b4132f052bd726316d202927c07e7f6c0f7188
sinisarudan <sinisa.rudan@gmail.com>
2023-11-17 03:03:45 +0100

    recovering changes lost due to merging of revert

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about">
+      <h1>FractalPM</h1>
+      <div>
+        Organize all your projects in an easy way.
+      </div>
+      <div>
+        Start managing your projects the right way.
+      </div>
+    </div>
+  </template>
+
+  <style lang="scss">
+  $mobile_breakpoint: 1024px;
+  @media (max-width: #{$mobile_breakpoint}) {
+    .about {
+      padding: 20px;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column; /* Stack items vertically */
+      gap: 20px; /* Set the desired padding between items */
+      align-items: center;
+    }
+  }
+  </style>
