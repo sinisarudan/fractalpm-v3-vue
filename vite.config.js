8f83a71f3815f2a445de89380e1392456f743a48
sinisarudan <sinisa.rudan@gmail.com>
2023-11-23 02:32:19 +0100

    adding support for Vuetify global and component SASS variables

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "url";
-// import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		// vuetify({ autoImport: true }),
-	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	}
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true, styles: { configFile: 'src/styles/settings.scss' } }) // for treeshaking reasons: https://vuetifyjs.com/en/features/treeshaking/
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
