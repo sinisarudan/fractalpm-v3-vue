11955ba187f23d3e1410c3be0340a3a45c977564
sinisarudan <sinisa.rudan@gmail.com>
2023-11-23 02:26:53 +0100

    themes added (colors, defaultsOptions (VBtn, VTextField)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,9 +2,24 @@
 
 // Styles
 import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
+// import 'vuetify/styles';
+import '@/styles/main.scss';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
+// const fractalPMDarkTheme = {
+//   dark: true,
+//   colors: {
+//     background: '#15202b',
+//     surface: '#15202b',
+//     primary: '#3f51b5',
+//     secondary: '#03dac6',
+//     error: '#044336',
+//     info: '#2196F3',
+//     success: '#4caf50',
+//     warning: '#fb8c00'
+//   }
+// };
 
 export default createVuetify({
   icons: {
@@ -13,5 +28,11 @@ export default createVuetify({
     sets: {
       mdi
     }
+    // theme: {
+    //   defaultTheme: 'fractalPMDarkTheme',
+    //   themes: {
+    //     fractalPMDarkTheme
+    //   }
+    // }
   }
 });
